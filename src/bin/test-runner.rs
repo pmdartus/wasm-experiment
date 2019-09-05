4b966a6114fe255340427c3af1f172ef9d5f75c7
Pierre-Marie Dartus <p.dartus@salesforce.com>
2019-09-05 06:25:43 +0200

    Simple JSON parsing

--- src/bin/test-runner.rs
+++ src/bin/test-runner.rs
@@ -1,10 +1,45 @@
 use std::env;
 use std::fs;
+use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
-use std::fs::File;
 
-use serde_json::{Value};
+use serde::Deserialize;
+use serde_json;
+
+#[derive(Deserialize, Debug)]
+struct TestManifest {
+    source_filename: String,
+    commands: Vec<TestCommand>,
+}
+
+#[derive(Deserialize, Debug)]
+struct TestCommand {
+    #[serde(alias = "type")]
+    command_type: String,
+    line: u32,
+    name: Option<String>,
+    filename: Option<String>,
+    action: Option<TestAction>,
+    text: Option<String>,
+    expected: Option<Vec<Value>>,
+}
+
+#[derive(Deserialize, Debug)]
+struct TestAction {
+    #[serde(alias = "type")]
+    action_type: String,
+    field: String,
+    module: Option<String>,
+    args: Option<Vec<Value>>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Value {
+    #[serde(alias = "type")]
+    value_type: String,
+    value: Option<String>,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,25 +49,18 @@ fn main() {
     };
 
     let test_dir = Path::new(dir);
-    let test_files: Vec<_> = fs::read_dir(test_dir)
+    let manifest_files: Vec<_> = fs::read_dir(test_dir)
         .unwrap()
         .map(|f| f.unwrap().path())
         .filter(|f| f.extension().unwrap() == "json")
         .collect();
 
-    for test_file in test_files {
-        println!("{:?}", test_file);
-        
-        let reader = BufReader::new(File::open(test_file).unwrap());
-        let data: Value = serde_json::from_reader(reader).unwrap();
+    for manifest_file in manifest_files {
+        println!("{:?}", manifest_file);
 
-        let tests = match &data["commands"] {
-            Value::Array(x) => x,
-            _ => unreachable!()
-        };
+        let reader = BufReader::new(File::open(manifest_file).unwrap());
+        let manifest: TestManifest = serde_json::from_reader(reader).unwrap();
 
-        for test in tests {
-            println!("type {:?}", test["type"])
-        }
+        println!("{:#?}", manifest);
     }
 }
